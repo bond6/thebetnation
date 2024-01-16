c0e67cf012bc30193555ea379931f0d7d06bb067
InnovativeGeneratedArt <123404714+CitrusPostharvest2023@users.noreply.github.com>
2024-01-16 08:21:05 +0200

    Add casino and sport sections updated homepage.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
+// vite.config.js
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "public",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        casino: resolve(__dirname, "casino/index.html"),
+        sport: resolve(__dirname, "sport/index.html"),
+      },
+    },
   },
-  // You can add more configurations here as needed
 });
